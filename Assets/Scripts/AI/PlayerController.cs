375d0e7740a6195f47409f3986157e16f8af0ec9
Romain Lavidalie <83449831+RomainLavidalie@users.noreply.github.com>
2021-11-22 16:54:22 +0100

    all around me are familiar faces

--- Assets/Scripts/AI/PlayerController.cs
+++ Assets/Scripts/AI/PlayerController.cs
@@ -7,21 +7,23 @@ using UnityEngine.AI;
 public class PlayerController : ClicAgentController
 {
     private float speed = 2.5f;
+    private NavMeshAgent movement;
 
     // Start is called before the first frame update
     void Start()
     {
+        movement = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         base.Update();
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButton("Jump"))
             speed = 5f;
         else
             speed = 2.5f;
-
-        GetComponent<NavMeshAgent>().speed = speed;
+        
+        movement.speed = speed;
     }
 }
